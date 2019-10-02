2bac3c5be522e5d53e51092266875798d9563da0
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-10-02 18:39:39 -0300

    Código javascript para mover o slider.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 import $ from 'jquery';
 
 $(function () {
-    console.log('HELLO WORLD!');
+    $('.next').on('click', function () {
+        var currentImg = $('.active');
+        var nextImg = currentImg.next();
+
+        if (nextImg.length > 0) {
+            currentImg.removeClass('active').css('z-index', -10);
+            nextImg.addClass('active').css('z-index', 10);
+        }
+    });
+
+    $('.prev').on('click', function () {
+        var currentImg = $('.active');
+        var prevImg = currentImg.prev();
+
+        if (prevImg.length > 0) {
+            currentImg.removeClass('active').css('z-index', -10);
+            prevImg.addClass('active').css('z-index', 10);
+        }
+    });
 });
 
