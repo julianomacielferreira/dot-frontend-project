9677a650ab5dbf76c28dfc72c72dbfc1ac575834
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-10-07 18:35:51 -0300

    Primeira parte da solução do bug de previous do slider.

--- js/app.js
+++ js/app.js
@@ -242,31 +242,50 @@ const FlexSliderMiddle = (() => {
 
             } else {
 
-                firstSliderVisible = lastSliderVisible - 3;
+                firstSliderVisible = lastSliderVisible + 1;
                 lastSliderVisible = threshold;
             }
+
+            console.log(`firstSliderVisible: ${firstSliderVisible}`);
+            console.log(`lastSliderVisible: ${lastSliderVisible}`);
         });
 
         $(".flex-prev-arrow").on('click', function () {
 
             let isToWrap = (firstSliderVisible == 0);
-            let threshold = totalSliderItems - 3;
+            let startAt = isToWrap ? (totalSliderItems - 3) : firstSliderVisible - 3;
+            let threshold = startAt + 3;
+
+            console.log(`isToWrap: ${isToWrap}`);
+            console.log(`startAt: ${startAt}`);
+            console.log(`threshold: ${threshold}`);
+            console.log(`firstSliderVisible: ${firstSliderVisible}`);
+            console.log(`lastSliderVisible: ${lastSliderVisible}`);
 
             $(".flex-slider-item").removeClass("active").each(function (index, element) {
 
                 const sliderItem = $(element);
-                const conditionToAddClass = (isToWrap && index > threshold && index <= totalSliderItems);
+                const conditionToAddClass = (isToWrap && index >= startAt && index < threshold);
 
                 if (conditionToAddClass) {
                     sliderItem.addClass("active");
                 }
+
+                const conditionWhenNotToWrap = (!isToWrap && index >= startAt && index < threshold);
+
+                if (conditionWhenNotToWrap) {
+                    sliderItem.addClass("active");
+                }
             });
 
-            if (isToWrap) {
+            // if (!isToWrap) {
 
-                firstSliderVisible = threshold;
-                lastSliderVisible = totalSliderItems;
-            }
+            //     firstSliderVisible = startAt + 1;
+            //     lastSliderVisible = threshold;
+
+            // } else {
+
+            // }
         });
     };
 
